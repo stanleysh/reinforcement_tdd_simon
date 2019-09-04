bd5dbfc04c1f7af365b9925af2125e4ad7881c68
Stanley Sham <stanley.sham94@gmail.com>
2019-09-04 16:17:26 -0400

    completed assignment

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,24 @@
+const echo = (word) =>{
+    return word;
+} 
+const shout = (word) =>{
+    return word.toUpperCase();
+}
+
+const repeat = (word, n) => {
+    newStr = word
+    for (var i = 0; i < n-1; i ++) {
+        newStr += ` ${word}`
+    }
+    return newStr;
+}
+
+const startOfWord = (word, n) =>{
+    return word.substring(0,n);
+}
+
+const firstWord = (phrase) => {
+    return phrase.split(' ')[0]
+}
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
